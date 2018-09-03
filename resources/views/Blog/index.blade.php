bce8cf56f3eea35badb81493d93f4ee20dcaa148
Fuadfikri <fuadfikri01@gmail.com>
2018-09-03 19:56:44 +0700

    tabel

--- resources/views/Blog/index.blade.php
+++ resources/views/Blog/index.blade.php
@@ -1,25 +1,33 @@
  {{ Session::get('message') }}
  <hr>
  @extends('Blog.master')
+<a href="{{ url('/blog/create') }}">
+  <button class="btn btn-warning col-1 offset-9 " >Tambah</button>
+</a>
  <center>
- @foreach($blogs as $blog)
-            <div class="card" style="width: 23rem;">
-            {{-- <img class="card-img-top" src=".../100px180/" alt="Card image cap"> --}}
-            <div class="card-body">
-                <h5 class="card-title">{{ $blog->title }}</h5>
-                <p class="card-text">{{ $blog->subject }}</p>
-                <p class="card-text">{{ date('F, d, Y', strtotime($blog->created_at)) }}</p>
-            <a href="{{url('/blog/'.$blog->slug)}}" class="btn btn-primary">Detail</a>
+   <br>
+     <table class="table table-hover table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Judul</th>
+      <th scope="col">Isi</th>
+      <th scope="col">action</th>
+    </tr>
+  </thead>
+  <tbody>
+@foreach($blogs as $blog)
+    <tr>
+    <th scope="row">{{ $blog->id }}</th>
+      <td>{{ $blog->title }}</td>
+      <td>{{ $blog->subject }}</td>
+      <td>  <a href="{{url('/blog/'.$blog->slug)}}" class="btn btn-primary">Detail</a>
             <a href="{{url('/blog/'.$blog->id)}}/edit" class="btn btn-success">edit</a>
-
-            <form action="{{ url('/blog/'.$blog->id) }}" method="post">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="hidden" name="_method" value="delete">
-                <input type="submit" value="Hapus" class="btn btn-danger">
-            </form>
-            </div>
-            </div>
-            <div style="margin:10px;"></div>
-@endforeach 
+            
+        </td>
+    </tr>
+@endforeach
+  </tbody>
+</table>
  </center>
  {{ $blogs->links() }}
--- resources/views/Blog/single.blade.php
+++ resources/views/Blog/single.blade.php
@@ -1,9 +1,19 @@
 @extends('Blog.master')
 
 @section('single')
-    <h2>halaman single</h2>
-    {{$blog->title }}
-    <br>
-    {{$blog->subject }}
+    <p>{{ date('F, d, Y', strtotime($blog->updated_at)) }}</p>
+    {{ date('F, d, Y', strtotime($blog->created_at)) }}
 
+    <h2>halaman single</h2>
+    <h2>{{$blog->title }}</h2>
+    <br>
+    <h4>{{$blog->subject }}</h4>
+    <br>
+    <form action="{{ url('/blog/'.$blog->id) }}" method="post">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="_method" value="delete">
+                <input type="submit" value="Hapus" class="btn btn-danger">
+            </form>
+
+ 
 @endsection
